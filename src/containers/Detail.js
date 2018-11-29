7d29572be257754645a09566e5ad2879e4fd306a
Jan Václavík <jvaclavik@gmail.com>
2018-11-29 17:55:14 +0100

    Add favorite button to Detail screen

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { SafeAreaView, Text, StyleSheet } from "react-native"
 import { connect } from "react-redux"
+import { RoundedButton } from "../components"
+import { onToggleFavorite } from "../redux/MoviesRedux"
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
@@ -14,19 +16,28 @@ class Detail extends React.PureComponent<null> {
   static navigationOptions = { title: "Movie detail" }
 
   render() {
-    const { movie } = this.props
+    const { movie, onToggleFavorite, isMovieFavorite } = this.props
     return (
       <SafeAreaView style={styles.container}>
         <Text>{movie.title}</Text>
+        <RoundedButton onPress={() => onToggleFavorite(movie.id)}>
+          {isMovieFavorite ? "Remove" : "Add"}
+        </RoundedButton>
       </SafeAreaView>
     )
   }
 }
-const mapStateToProps = (state, props) => ({
-  movie: getMovieById(state, props.navigation.state.params.movieId),
-})
+const mapStateToProps = (state, props) => {
+  const { movieId } = props.navigation.state.params
+  return {
+    movie: getMovieById(state, movieId),
+    isMovieFavorite: state.movies.favorites.indexOf(movieId) > -1,
+  }
+}
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {
+  onToggleFavorite,
+}
 
 export default connect(
   mapStateToProps,
--- src/redux/MoviesRedux.js
+++ src/redux/MoviesRedux.js
@@ -1,10 +1,15 @@
 export const initialState = {
   items: [],
+  favorites: [],
 }
 
 export const onGetMovies = movies => ({
   type: "ON_GET_MOVIES",
   movies,
+})
+export const onToggleFavorite = movieId => ({
+  type: "ON_TOGGLE_FAVORITE",
+  movieId,
 })
 
 export const reducer = (state = initialState, action) => {
@@ -13,6 +18,14 @@ export const reducer = (state = initialState, action) => {
       return {
         ...state,
         items: action.movies,
+      }
+    case "ON_TOGGLE_FAVORITE":
+      return {
+        ...state,
+        favorites:
+          state.favorites.indexOf(action.movieId) > -1
+            ? state.favorites.filter(favorite => favorite !== action.movieId)
+            : [...state.favorites, action.movieId],
       }
     default:
       return state
