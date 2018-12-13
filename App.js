8257be42731b681baabcd0ec1a00f4f000f73025
Jan Václavík <jvaclavik@gmail.com>
2018-12-13 18:27:49 +0100

    Add redux-persist

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import Expo from "expo"
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware, combineReducers } from "redux"
 import { createLogger } from "redux-logger"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import { PersistGate } from "redux-persist/integration/react"
+
 // redux
 import { reducer as moviesReducer } from "./src/redux/MoviesRedux"
 
@@ -16,19 +20,34 @@ const logger = createLogger({ collapsed: true })
 const middleware = []
 const initialState = {}
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["movies"],
+}
+
 middleware.push(logger)
 
-const store = createStore(
+const persistedReducer = persistReducer(
+  persistConfig,
   combineReducers({ movies: moviesReducer }),
+)
+
+const store = createStore(
+  persistedReducer,
   initialState,
   applyMiddleware(...middleware),
 )
+
+const persistor = persistStore(store)
 
 export default class App extends React.PureComponent<null> {
   render() {
     return (
       <Provider store={store}>
-        <Navigator />
+        <PersistGate loading={null} persistor={persistor}>
+          <Navigator />
+        </PersistGate>
       </Provider>
     )
   }
